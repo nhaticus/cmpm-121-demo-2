b50a1032eab09ccb608b393c0ac67d1ffb06abb0
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-22 23:50:45 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import "./style.css";
 
-const APP_NAME = "Finally decided to start huh";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+const APP_NAME = "The Drawing board";
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const APP_TITTLE = document.createElement("h1");
+APP_TITTLE.innerText = APP_NAME;
+app.appendChild(APP_TITTLE);
+
+const canvas = document.createElement('canvas');
+canvas.height = 256, canvas.width = 256;
+canvas.classList.add("canvas");
+app.append(canvas);
