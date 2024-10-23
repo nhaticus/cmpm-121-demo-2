52ca7010f174e4a90829008315ca7d74985ea6b7
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-22 23:53:50 -0700

    neater?

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 const APP_NAME = "The Drawing board";
 document.title = APP_NAME;
 
-const APP_TITTLE = document.createElement("h1");
-APP_TITTLE.innerText = APP_NAME;
-app.appendChild(APP_TITTLE);
+const appTitle = document.createElement("h1");
+appTitle.innerText = APP_NAME;
+app.appendChild(appTitle);
 
 const canvas = document.createElement('canvas');
-canvas.height = 256, canvas.width = 256;
+canvas.height = 256;
+canvas.width = 256;
 canvas.classList.add("canvas");
 app.append(canvas);
