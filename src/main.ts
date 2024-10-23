6b38cfd239ad4102616a71558689036d48d3bddd
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-23 00:59:41 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,26 @@ canvas.height = 256;
 canvas.width = 256;
 canvas.classList.add("canvas");
 app.append(canvas);
+
+const context = canvas.getContext("2d");
+const cursor = { isPressed: false, x: 0, y:0 };
+canvas.addEventListener("mousedown", (tmp) => {
+    cursor.isPressed = true;
+    cursor.x = tmp.offsetX;
+    cursor.y = tmp.offsetY;
+})
+
+canvas.addEventListener("mousemove", (tmp) => {
+    if (cursor.isPressed) {
+        context?.beginPath();
+        context?.moveTo(cursor.x, cursor.y);
+        context?.lineTo(tmp.offsetX, tmp.offsetY)
+        context?.stroke();
+        cursor.x = tmp.offsetX;
+        cursor.y = tmp.offsetY;
+    }
+})
+
+canvas.addEventListener("mouseup", () => {
+    cursor.isPressed = false;
+})
