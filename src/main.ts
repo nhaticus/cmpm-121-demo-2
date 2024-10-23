dc626f731d6576bb8593086fca82e8ef1293a0b4
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-23 11:13:03 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -15,20 +15,43 @@ canvas.width = 256;
 canvas.classList.add("canvas");
 app.appendChild(canvas);
 
+const strokes: Array<Array<{ x: number, y: number }>> = [];
+const eventObserver = new Event("drawing-changed");
+
+canvas.addEventListener("drawing-changed", () => {
+    //console.log(strokes);
+    context?.clearRect(0, 0, canvas.width, canvas.height);
+
+    for ( const stroke of strokes) {
+        if (stroke.length > 1) {
+            context?.beginPath();
+            context?.moveTo(stroke[0].x, stroke[0].y);
+            for ( const point of stroke) {
+                context?.lineTo(point.x, point.y);
+            }
+            context?.stroke();
+        }
+    }
+})
+
+
 const context = canvas.getContext("2d");
 const cursor = { isPressed: false, x: 0, y:0 };
+
+
 canvas.addEventListener("mousedown", (tmp) => {
     cursor.isPressed = true;
     cursor.x = tmp.offsetX;
     cursor.y = tmp.offsetY;
+
+    strokes.push([{ x: cursor.x, y: cursor.y}]);
 })
 
 canvas.addEventListener("mousemove", (tmp) => {
     if (cursor.isPressed) {
-        context?.beginPath();
-        context?.moveTo(cursor.x, cursor.y);
-        context?.lineTo(tmp.offsetX, tmp.offsetY)
-        context?.stroke();
+        strokes[strokes.length - 1].push({ x: tmp.offsetX, y: tmp.offsetY});
+
+        canvas.dispatchEvent(eventObserver);
 
         cursor.x = tmp.offsetX;
         cursor.y = tmp.offsetY;
@@ -44,4 +67,6 @@ clearButton.innerHTML= "clear";
 app.appendChild(clearButton);
 clearButton.addEventListener("click", () => {
     context?.clearRect(0, 0, canvas.width, canvas.height);
+    strokes.length = 0;
+    //console.log(strokes);
 })
