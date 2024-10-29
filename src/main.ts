be86e7a792da849d0b13e04c4db730d33995ffd7
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-29 09:57:29 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,98 +1,18 @@
 import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
-
-const APP_NAME = "The Drawing board";
-document.title = APP_NAME;
-
-const _appTitle = createAppTitle(APP_NAME);
-
-const canvas = createCanvas(256, 256, "canvas");
-
-const lines: Array<Array<{ x: number; y: number }>> = [];
-const redoLines: Array<Array<{ x: number; y: number }>> = [];
-
-const eventObserver = new Event("drawing-changed");
-
-const ctx = canvas.getContext("2d");
-const cursor = { isPressed: false, x: 0, y: 0 };
-
-canvas.addEventListener("drawing-changed", () => {
-  ctx?.clearRect(0, 0, canvas.width, canvas.height);
-
-  for (const line of lines) {
-    if (line.length > 1) {
-      ctx?.beginPath();
-      ctx?.moveTo(line[0].x, line[0].y);
-      for (const point of line) {
-        ctx?.lineTo(point.x, point.y);
-      }
-      ctx?.stroke();
-    }
-  }
-});
-
-canvas.addEventListener("mousedown", (tmp) => {
-  cursor.isPressed = true;
-  cursor.x = tmp.offsetX;
-  cursor.y = tmp.offsetY;
-
-  lines.push([{ x: cursor.x, y: cursor.y }]);
-
-  redoLines.length = 0;
-});
-
-canvas.addEventListener("mousemove", (tmp) => {
-  if (cursor.isPressed) {
-    lines[lines.length - 1].push({ x: tmp.offsetX, y: tmp.offsetY });
-
-    canvas.dispatchEvent(eventObserver);
-
-    cursor.x = tmp.offsetX;
-    cursor.y = tmp.offsetY;
-  }
-});
-
-canvas.addEventListener("mouseup", () => {
-  cursor.isPressed = false;
-});
-
-app.append(document.createElement("br"));
-const _clearButton = createButton("clear", clearHandler);
-const _undoButton = createButton("undo", undoHandler);
-const _redoButton = createButton("redo", redoHandler);
-
-function clearHandler() {
-  ctx?.clearRect(0, 0, canvas.width, canvas.height);
-  lines.length = 0;
-}
-
-function undoHandler() {
-  if (lines.length > 0) {
-    const lastLine = lines.pop();
-    if (lastLine) {
-      redoLines.push(lastLine);
-    }
-    canvas.dispatchEvent(eventObserver);
-  }
-}
-
-function redoHandler() {
-  if (redoLines.length > 0) {
-    const redoLine = redoLines.pop();
-    if (redoLine) {
-      lines.push(redoLine);
-    }
-    canvas.dispatchEvent(eventObserver);
-  }
-}
-
 function createAppTitle(title: string): HTMLElement {
   const tmpTitle = document.createElement("h1");
   tmpTitle.innerHTML = title;
   app.appendChild(tmpTitle);
   return tmpTitle;
 }
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const APP_NAME = "The Drawing board";
+document.title = APP_NAME;
+
+const _appTitle = createAppTitle(APP_NAME);
 
 function createCanvas(
   width: number,
@@ -107,6 +27,77 @@ function createCanvas(
   return tmpCanvas;
 }
 
+const canvas = createCanvas(256, 256, "canvas");
+const ctx = canvas.getContext("2d");
+
+interface Command {
+  points: Array<{ x: number; y: number }>;
+  drag(point: { x: number; y: number }): void;
+  display(ctx: CanvasRenderingContext2D): void;
+}
+
+function createCommand(): Command {
+  return {
+    points: [],
+
+    drag(point: { x: number; y: number }): void {
+      this.points.push(point);
+    },
+
+    display(ctx: CanvasRenderingContext2D) {
+      ctx.beginPath();
+      const { x, y } = this.points[0];
+      if (this.points.length > 0) {
+        ctx.moveTo(x, y);
+        for (const { x, y } of this.points) {
+          ctx.lineTo(x, y);
+        }
+      }
+      ctx.stroke();
+    },
+  };
+}
+
+const commands: Command[] = [];
+const redoCommands: Command[] = [];
+const bus = new EventTarget();
+
+function eventTrigger(name: string) {
+  bus.dispatchEvent(new Event(name));
+}
+
+bus.addEventListener("drawing-changed", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const command of commands) {
+    command.display(ctx!);
+  }
+});
+
+let currentCommand: Command | null = null;
+
+canvas.addEventListener("mousedown", (tmp) => {
+  currentCommand = createCommand();
+  currentCommand.drag({ x: tmp.offsetX, y: tmp.offsetY });
+  commands.push(currentCommand);
+  redoCommands.length = 0;
+
+  eventTrigger("drawing-changed");
+});
+
+canvas.addEventListener("mousemove", (tmp) => {
+  if (tmp.buttons === 1 && currentCommand) {
+    currentCommand.drag({ x: tmp.offsetX, y: tmp.offsetY });
+
+    eventTrigger("drawing-changed");
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  currentCommand = null;
+  eventTrigger("drawing-changed");
+});
+
 function createButton(
   buttonText: string,
   eventHandler: () => void
@@ -116,4 +107,32 @@ function createButton(
   tmpButton.addEventListener("click", eventHandler);
   app.appendChild(tmpButton);
   return tmpButton;
+}
+
+app.append(document.createElement("br"));
+const _clearButton = createButton("clear", clearHandler);
+const _undoButton = createButton("undo", undoHandler);
+const _redoButton = createButton("redo", redoHandler);
+
+function clearHandler() {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+  commands.length = 0;
+  eventTrigger("drawing-changed");
+}
+
+function undoHandler() {
+  if (commands.length > 0) {
+    redoCommands.push(commands.pop()!);
+    eventTrigger("drawing-changed");
+  }
+}
+
+function redoHandler() {
+  if (redoCommands.length > 0) {
+    const redoLine = redoCommands.pop();
+    if (redoLine) {
+      commands.push(redoLine);
+    }
+    eventTrigger("drawing-changed");
+  }
 }
